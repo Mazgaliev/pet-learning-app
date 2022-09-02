6e27333f0992dafdc476d76e81d7f2ede93470cb
Mite <mite_mazgaliev@live.com>
2022-09-02 21:38:06 +0200

    Initial commit

---
+++ src/main/java/com/example/petlearninapp/PetLearninAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.petlearninapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLearninAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetLearninAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petlearninapp/PetLearninAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petlearninapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLearninAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
