53203862d2cbbbf75f03cb8712767e5e5c0e464d
Mite <mite_mazgaliev@live.com>
2022-09-03 13:57:23 +0200

    Initial commit

--- src/main/java/com/example/petlearninapp/web/HomeController.java
+++ src/main/java/com/example/petlearninapp/web/HomeController.java
@@ -4,11 +4,13 @@ import com.example.petlearninapp.service.CourseService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller()
+@Controller
 @RequestMapping(value = {"/home", "/"})
+@CrossOrigin("*")
 public class HomeController {
 
     private final CourseService courseService;
